ef5ca3711ea0e23156d2272f3e2fa922b0839e8d
JingHong Huang <jinghong@bu.edu>
2022-11-04 15:43:46 -0400

    editing files and setting up the multi-page

--- 411-latte/src/App.js
+++ 411-latte/src/App.js
@@ -1,32 +1,26 @@
 import React from "react"; 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Routes, useNavigate, BrowserRouter} from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+// import Contact from "./pages/Contact";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Wrapper from "./components/Wrapper";
 import "./App.css";
 import logo from "./logo.svg";
 
 
 function App() {
     return (
-        <Router>
         <div className = "I'm Not Latte">
-            <Navbar />
-            <Wrapper>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/contact" component={Contact} />
-            </Switch>
-            </Wrapper>
-            <Footer />
+            <h1 > Testing something </h1>
+        <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />}/>
+        </Routes>
         </div>
-        </Router>
     );
-    }
+}
 
 export default App;
 
---
+++ 411-latte/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Button} from "react-bootstrap";
+import {useNavigate} from 'react-router-dom';
+
+const Home = () => {
+  let navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate('about');
+  };
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <Button onClick={handleOnClick} >About</Button>
+    </div>
+  );
+}
+
+export default Home;
