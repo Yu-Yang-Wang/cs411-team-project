872c8ce653c7d11c1ceacb86aaeb5572028fc75f
JingHong Huang <jinghong@bu.edu>
2022-11-10 15:32:24 -0500

    added search bar template

--- 411-latte/src/App.js
+++ 411-latte/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes, useNavigate, BrowserRouter} from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+import SearchBar from "./pages/SearchBar";
 // import Contact from "./pages/Contact";
 // import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
@@ -13,12 +15,14 @@ import logo from "./logo.svg";
 function App() {
     return (
         <div className = "I'm Not Latte">
-            <h1 > Testing something </h1>
         <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />}/>
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/searchbar" element={<SearchBar />} />
         </Routes>
         </div>
+
     );
 }
 
