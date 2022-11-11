13b5f4bcac3d5d911d8140d27a51133a855d7897
JingHong Huang <jinghong@bu.edu>
2022-11-11 01:38:22 -0500

    setting up the backend enviro

--- 411-latte/flask/app.py
+++ 411-latte/flask/app.py
@@ -1,25 +1,10 @@
-from flask import Flask, request
-import scraper, json
 from flask_cors import CORS, cross_origin
+from flask_restx import Api
+from flask import Flask, jsonify
+from apis import api
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+api.init_app(app)
 
-@app.route("/")
-def hello_world():
-   return "<p>Hello, World!</p>"
-
-# takes form input and returns price results
-@app.route('/search', methods=['GET', 'POST'])
-@cross_origin()
-def getProduct():
-   data = json.loads(request.data)
-   print("request received json: "+str(data))
-   product = data['product']
-   user = data['user']
-   print("product name="+product+"|user="+user)
-   return scraper.getData(product)
-
-
-print(hello_world)
+app.run(debug=True)
