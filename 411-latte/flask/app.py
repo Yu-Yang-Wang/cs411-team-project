f7cef96277c786bb9e0c66859edfba58d547a6f3
JingHong Huang <jinghong@bu.edu>
2022-12-03 18:30:22 -0500

    adding files for backend

--- 411-latte/flask/app.py
+++ 411-latte/flask/app.py
@@ -1,10 +1,13 @@
 from flask_cors import CORS, cross_origin
 from flask_restx import Api
 from flask import Flask, jsonify
-from flask import api
+from api import apis as api 
 
 app = Flask(__name__)
 CORS(app)
 api.init_app(app)
 
-app.run(debug=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
